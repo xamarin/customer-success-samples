a78b7e73dda035346ad033ea0fbe8bc0eb013eae
jon-davis-xamarin <jon.davis>
2015-05-26 19:44:53 -0700

    Cleanup

--- samples/Xamarin.iOS/ScanAPI-Binding/ScanAPI/libSoftScanWrapper.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using ObjCRuntime;
-
-[assembly: LinkWith ("libSoftScanWrapper.a", LinkTarget.ArmV7 | LinkTarget.Simulator64 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator, Frameworks = "AudioToolbox AVFoundation CoreGraphics SystemConfiguration QuartzCore CoreImage MultipeerConnectivity Security CFNetwork UIKit Foundation ExternalAccessory", LinkerFlags = "", IsCxx = true, SmartLink = true, ForceLoad = true)]
