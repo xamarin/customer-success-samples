bfd4d822828a7b33b790db41e36479bf3b538c6d
michael-watson <michael.watson@xamarin.com>
2015-06-29 17:07:05 -0700

    Initial Commit

---
+++ samples/Xamarin.Forms/FormsTrays/FormsTrays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FormsTrays
+{
+	public class App : Application
+	{
+		public static double ScreenHeight;
+		public static double ScreenWidth;
+
+		public App ()
+		{
+			TabbedPage page = new TabbedPage {
+				Children = {
+					new TopPage(),
+					new BottomPage(),
+					new RightPage(),
+					new LeftPage(),
+				}
+			};
+
+			// The root page of your application
+			MainPage = page;
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ samples/Xamarin.Forms/FormsTrays/PagesForExample/BottomPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FormsTrays
+{
+	public class BottomPage : ContentPage
+	{
+		Tray tray;
+
+		public BottomPage ()
+		{
+			Title = "Bottom";
+
+			tray = new Tray (TrayOrientation.Bottom, 0.5) {
+				VerticalOptions = LayoutOptions.End,
+			};
+
+			tray.Content = new ContentView {
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				BackgroundColor = Color.Red
+			};
+
+			Button switchTray = new Button { 
+				Text = "Open/Close Tray",
+				WidthRequest = 0.5 * App.ScreenWidth
+			};
+			switchTray.Clicked += ToggleTray;
+
+			AbsoluteLayout layout = new AbsoluteLayout () {
+				HeightRequest = App.ScreenHeight,
+				WidthRequest = App.ScreenWidth,
+				BackgroundColor = Color.Yellow
+			};
+
+			layout.Children.Add (switchTray, new Rectangle (App.ScreenWidth * 0.25, 100, switchTray.WidthRequest, 50));
+			layout.Children.Add (tray, new Rectangle (0, App.ScreenHeight, App.ScreenWidth, tray.HeightRequest));
+
+			Content = layout;
+		}
+
+		void ToggleTray(object sender, EventArgs e)
+		{
+			if (tray.IsOpen) {
+				tray.Close ();
+			} else if (!tray.IsOpen) {
+				tray.Open ();
+			}
+		}
+	}
+}
