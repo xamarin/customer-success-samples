097eaa363498685a26786c93ba4a2a9863c6cdb1
michael-watson <michael.watson@xamarin.com>
2015-07-09 13:44:33 -0700

    Initial Upload

---
+++ samples/Xamarin.Forms/TabbedRendererDemo/TabbedPageDemo/TabbedPageDemo.iOS/TabbedPageCustomRenderer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using TabbedPageDemo.iOS;
+
+using UIKit;
+
+[assembly:ExportRenderer(typeof(TabbedPage),typeof(TabbedPageCustomRenderer))]
+
+namespace TabbedPageDemo.iOS
+{
+	public class TabbedPageCustomRenderer : TabbedRenderer
+	{
+		UITabBarController tabbedController;
+		UISwipeGestureRecognizer rightGesture, leftGesture;
+		bool isInitialized = false;
+
+		protected override void OnElementChanged (VisualElementChangedEventArgs e)
+		{
+			base.OnElementChanged (e);
+
+			if (e.NewElement != null) {
+				tabbedController = (UITabBarController)ViewController;
+			}
+
+			if (!isInitialized) {
+
+				rightGesture = new UISwipeGestureRecognizer (swipe => {
+					//Check to make sure we aren't at the last view
+					Console.WriteLine ("Swipe Left");
+					if(this.SelectedIndex != ViewControllers.Length - 1)
+						tabbedController.SelectedViewController = ViewControllers [this.SelectedIndex + 1];
+				}) {
+					Direction = UISwipeGestureRecognizerDirection.Left
+				};
+
+				leftGesture = new UISwipeGestureRecognizer (swipe => {
+					//Check to make sure we aren't at the first view
+					Console.WriteLine ("Swipe Right");
+					if(this.SelectedIndex != 0)
+						tabbedController.SelectedViewController = ViewControllers [this.SelectedIndex - 1];
+				}) {
+					Direction = UISwipeGestureRecognizerDirection.Right
+				};
+
+				View.AddGestureRecognizer (rightGesture);
+				View.AddGestureRecognizer (leftGesture);
+
+				isInitialized = true;
+			}
+		}
+	}
+}
---
+++ samples/Xamarin.Forms/TabbedRendererDemo/TabbedPageDemo/TabbedPageDemo/MonkeyTabbedPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TabbedPageDemo
+{
+    public partial class MonkeyTabbedPage
+    {
+        public MonkeyTabbedPage()
+        {
+            InitializeComponent();
+
+            this.ItemsSource = MonkeyDataModel.All;
+        }
+    }
+}
