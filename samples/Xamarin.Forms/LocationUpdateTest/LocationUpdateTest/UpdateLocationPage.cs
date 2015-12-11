d31946411ebd31f726b7cad925ad2589afb1d2a3
Michael Watson <michaelw@michaelw-mac.local>
2015-12-11 15:40:36 -0800

    LocationUpdateTest

---
+++ samples/Xamarin.Forms/LocationUpdateTest/LocationUpdateTest/UpdateLocationPage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using Geolocator.Plugin;
+using Geolocator.Plugin.Abstractions;
+
+namespace LocationUpdateTest
+{
+	public class UpdateLocationPage : ContentPage
+	{
+		Label longitude;
+		Label latitude;
+		IGeolocator locator;
+
+		public UpdateLocationPage ()
+		{
+			locator = CrossGeolocator.Current;
+			locator.DesiredAccuracy = 5;
+
+			longitude = new Label ();
+			latitude = new Label ();
+
+			Content = new StackLayout {
+				HorizontalOptions = LayoutOptions.Center,
+				VerticalOptions = LayoutOptions.Center,
+				Children = { 
+					longitude,
+					latitude
+				}
+			};
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+
+			locator.StartListening (50, 1);
+
+			locator.PositionChanged += async (object sender, PositionEventArgs e) => {
+				var position = await locator.GetPositionAsync (timeout: 10000);
+				longitude.Text = "Longitude: " + position.Longitude.ToString();
+				latitude.Text = "Latitude: " + position.Longitude.ToString();
+			};
+		}
+	}
+}
---
+++ samples/Xamarin.Forms/LocationUpdateTest/README.md
@@ -0,0 +1,7 @@
+#Forms Location Update	
+This sample utilizes the GeoLocation plugin to provide the current location. The UI will update anytime the GPS coordinate location is updated. This is a very basic sample to demonstrate the plugin in an example.
+
+####Author:
+**Michael Watson**  
+**Customer Success Engineer**  
+**Xamarin.Forms Ninja**  
