7b656d396bd6a29b40193fb5e52994acdb4692d6
Glenn Wester <glennwester@Glenns-MacBook-Pro-2.local>
2016-02-08 10:47:21 -0800

    Committing Nasa Missions
    Nasa Missions is a simple Xamarin.Forms sample to demonstrate
    populating a Form style TableView with some space data.

---
+++ samples/Xamarin.Forms/NasaMissions/NasaMissions/Pages/Menu.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NasaMissions
+{
+	public class Menu : ContentPage
+	{
+		public Menu ()
+		{
+
+			Content = new TableView () {
+				BackgroundColor = Color.Black,
+
+				Intent = TableIntent.Form,
+				Root = new TableRoot () {
+					new TableSection ("") 
+					{						
+						new KeyValueCell ("Xamarin.Forms", "Nasa Planet Sample"),
+					},				
+					new TableSection ("Deep Space") 
+					{						
+						
+						new ImageCell () 
+						{
+							Text = "Voyager",
+							TextColor = Color.White,
+							DetailColor = Color.Blue,
+							Detail = "Launched 10/24/2005",
+							ImageSource = new FileImageSource () { File = "Location.png"  },
+						},
+						new ImageCell () 
+						{
+							Text = "New Horizons",
+							TextColor = Color.White,
+							Detail = "Launched 10/24/2005",
+							DetailColor = Color.Gray,
+							ImageSource = new FileImageSource () { File = "Location.png"  },
+						},
+							
+					},
+					new TableSection("Planets")
+					{
+						
+						new ImageCell() 
+						{
+							Text = "Mercury",
+							Detail = "Detail",
+							TextColor = Color.White,
+
+							ImageSource = new FileImageSource () { File = @"Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Venus",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Earth",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Mars",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Jupiter",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Saturn",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Uranus",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+						new ImageCell() 
+						{
+							Text = "Neptune",
+							Detail = "Detail",
+							TextColor = Color.White,
+							ImageSource = new FileImageSource () { File = "Planet100.png"}, 
+
+						},
+							
+					}
+
+
+
+
+				}
+
+
+			};
+					
+
+		}
+	}
+}
---
+++ samples/Xamarin.Forms/NasaMissions/README.md
@@ -0,0 +1,14 @@
+Nasa Missions
+=====
+
+Nasa Missions is a simple Xamarin.Forms sample to demonstrate populating a Form style TableView with some space data. 
+
+Sample includes a able with image cells with png icons. This sample is builds and deploys on iOS and Android. 
+
+
+![screenshot](https://raw.githubusercontent.com/xamarin/customer-success-samples/GlennSampleAppsFeb2016/samples/Xamarin.iOS/SanAndreasMap/Screenshots/sanAndres1.png "iOS")
+
+Authors
+-------
+
+Glenn Wester
