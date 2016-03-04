134b8f3c8076f63df6a55c6b64aec9d01b66839f
Glenn Wester <glennwester@Glenns-MacBook-Pro-2.local>
2016-03-04 14:07:52 -0800

    Adding InvokeExample
    adding .gitignore file to NasaMissions

---
+++ samples/XamarinTestCloud/InvokeExample/InvokeExample.UITests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.iOS;
+using Xamarin.UITest.Queries;
+
+
+namespace InvokeExample.UITests
+{
+	[TestFixture]
+	public class Tests
+	{
+		iOSApp app;
+
+		[SetUp]
+		public void BeforeEachTest ()
+		{
+			app = ConfigureApp.iOS.StartApp ();
+		}
+
+		[Test]
+		public void ViewIsDisplayed ()
+		{
+			// Example of Invoking selectRow in the native UIPickerView to set the value to the 4th row (0-based array)
+			var invokingHere = app.Query(e => e.Id("PizzaPicker").Index(0).Invoke("selectRow", 3 , "inComponent", 3, "animated", true));
+
+		}
+			
+
+	}
+}
+
+
---
+++ samples/XamarinTestCloud/InvokeExample/InvokeExample/ViewController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+using UIKit;
+using Foundation;
+using System.Collections.Generic;
+
+namespace InvokeExample
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			PizzaPicker.AccessibilityIdentifier = "PizzaPicker";
+			PizzaPicker.Model = new StatusPickerViewModel ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+	}
+
+
+	public class StatusPickerViewModel : UIPickerViewModel
+	{
+		public override nint GetComponentCount (UIPickerView picker)
+		{
+			return 1;
+		}
+
+		public override nint GetRowsInComponent (UIPickerView pickerView, nint component)
+		{
+			return 5;
+
+		}
+
+		public override string GetTitle (UIPickerView picker, nint row, nint component)
+		{
+
+			return "Component " + row.ToString();
+		}
+
+	}
+
+
+
+}
+
