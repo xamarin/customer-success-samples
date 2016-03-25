6bd45a45eb7d9f44d0d654dc99f67651e9512064
brminnick <brandon.r.minnick@gmail.com>
2016-03-25 15:23:50 -0700

    Updated DLLs

--- samples/Xamarin.Forms/SimpleUITestApp/Droid/MainActivity.cs
+++ samples/Xamarin.Forms/SimpleUITestApp/Droid/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 namespace SimpleUITestApp.Droid
 {
 	[Activity (Theme = "@android:style/Theme.Material.Light", Label = "SimpleUITestApp", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
 	{
 		protected override void OnCreate (Bundle bundle)
 		{
