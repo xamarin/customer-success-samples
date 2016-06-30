7921fe26bd9c82b41ca9773453a0daa72ec20b03
brminnick <brandon.r.minnick@gmail.com>
2016-06-29 17:56:59 -0700

    Updated Folder Name, MetaData & NuGet Packages

---
+++ samples/Xamarin.Forms/SecuritySampleApp/iOS/Custom Renderers/ViewCellItemSelectedCustomRenderer.cs
@@ -0,0 +1,23 @@
+﻿using UIKit;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using SecuritySampleApp.iOS;
+
+[assembly: ExportRenderer(typeof(ViewCell), typeof(ViewCellItemSelectedCustomRenderer))]
+
+namespace SecuritySampleApp.iOS
+{
+    public class ViewCellItemSelectedCustomRenderer : ViewCellRenderer
+    {
+        public override UITableViewCell GetCell(Cell item, UITableViewCell reusableCell, UITableView tv)
+        {
+            var cell = base.GetCell(item, reusableCell, tv);
+
+            cell.SelectionStyle = UITableViewCellSelectionStyle.None;
+
+            return cell;
+        }
+    }
+}
