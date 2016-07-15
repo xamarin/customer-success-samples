0cfe615fee1e34e770c849ca1a0abcd811ae449e
brminnick <brandon.r.minnick@gmail.com>
2016-07-15 13:31:40 -0700

    Added EntryUITest app

---
+++ samples/Xamarin.Forms/EntryUITest/Entry_UITest.UITests/AppInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.UITest;
+
+namespace Entry_UITest.UITests
+{
+	public class AppInitializer
+	{
+		public static IApp StartApp(Platform platform)
+		{
+			if (platform == Platform.Android)
+			{
+				return ConfigureApp.Android.EnableLocalScreenshots().StartApp();
+			}
+
+			return ConfigureApp.iOS.EnableLocalScreenshots().StartApp();
+		}
+	}
+}
+
---
+++ samples/Xamarin.Forms/EntryUITest/iOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using UIKit;
+using Foundation;
+
+using Xamarin.Forms;
+
+namespace Entry_UITest.iOS
+{
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init();
+			global::Xamarin.Forms.Forms.ViewInitialized += (object sender, ViewInitializedEventArgs e) =>
+			{
+				// http://developer.xamarin.com/recipes/testcloud/set-accessibilityidentifier-ios/
+				if (null != e.View.AutomationId)
+				{
+					e.NativeView.AccessibilityIdentifier = e.View.AutomationId;
+				}
+			};
+
+			// Code for starting up the Xamarin Test Cloud Agent
+			#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+			#endif
+
+			LoadApplication(new App());
+
+			return base.FinishedLaunching(app, options);
+		}
+	}
+}
+
