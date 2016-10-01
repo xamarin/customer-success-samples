d17e4adf4dae4e1e8d12945953b7dbc4231908c0
brminnick <brandon.r.minnick@gmail.com>
2016-10-01 13:11:41 -0700

    Updated WebViewUITestSampleApp

--- samples/XamarinTestCloud/WebViewUITestApp/WebViewUITestApp.UITest/Tests.cs
+++ samples/XamarinTestCloud/WebViewUITestApp/WebViewUITestApp.UITest/Tests.cs
@@ -4,22 +4,22 @@ using Xamarin.UITest;
 namespace WebViewUITestApp.UITest
 {
 	[TestFixture(Platform.Android)]
-    [TestFixture(Platform.iOS)]
-    public class Tests
-    {
-        IApp app;
-        Platform platform;
+	[TestFixture(Platform.iOS)]
+	public class Tests
+	{
+		IApp app;
+		Platform platform;
 
-        public Tests(Platform platform)
-        {
-            this.platform = platform;
-        }
+		public Tests(Platform platform)
+		{
+			this.platform = platform;
+		}
 
-        [SetUp]
-        public void BeforeEachTest()
-        {
-            app = AppInitializer.StartApp(platform);
-        }
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			app = AppInitializer.StartApp(platform);
+		}
 
 		[Ignore]
 		[Test]
@@ -30,15 +30,16 @@ namespace WebViewUITestApp.UITest
 			app.Repl();
 		}
 
-        [Test]
-        public void ViewXamarinWebsite()
-        {
-			app.Tap(x => x.Css("SPAN.menu"));
-			app.Screenshot("Menu Tapped");
-			app.Tap(x => x.Css("A").Index(2));
-			app.Screenshot("Products Tapped");
-			app.Tap(x => x.Css("A.action-button.blue.solid"));
-			app.Screenshot("Learn More About Xamarin Tapped");
+		[Test]
+		public void ViewXamarinWebsite()
+		{
+			app.Tap(x => x.Class("Xamarin_Forms_Platform_iOS_WebViewRenderer").Css("SPAN.menu"));
+			app.Screenshot("Tapped on view with class: Xamarin_Forms_Platform_iOS_WebViewRenderer");
+			app.Tap(x => x.Class("Xamarin_Forms_Platform_iOS_WebViewRenderer").XPath("//DIV[@class=\"navigation in\"]/UL/LI[@class=\"show-on-mobile\"][1]/A"));
+			app.Screenshot("Tapped on view with class: Xamarin_Forms_Platform_iOS_WebViewRenderer");
+			app.ScrollDownTo(x => x.Class("Xamarin_Forms_Platform_iOS_WebViewRenderer").XPath("//A[@class=\"microsoft-logo\"]/IMG"));
+			app.Tap(x => x.Class("Xamarin_Forms_Platform_iOS_WebViewRenderer").XPath("//A[@class=\"microsoft-logo\"]/IMG"));
+			app.Screenshot("Tapped on view with class: Xamarin_Forms_Platform_iOS_WebViewRenderer");
 		}
 	}
 }
--- samples/XamarinTestCloud/WebViewUITestApp/WebViewUITestApp/WebViewUITestApp.Droid/Resources/Resource.Designer.cs
+++ samples/XamarinTestCloud/WebViewUITestApp/WebViewUITestApp/WebViewUITestApp.Droid/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace WebViewUITestApp.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::WebViewUITestApp.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Attribute
